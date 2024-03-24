4cc741de6cff19630bb0151c8f6b1378c2e992c9
elisemacabou <elise.macabou@gmail.com>
2024-03-24 16:04:41 -0700

    LM741 updated

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ import re, tempfile, os
 import pandas as pd
 from constants import AD590_NETLIST_TEMPLATE_PATH, XYCE_EXE_PATH, LM741_NETLIST_PATH , IOS_VOS_IB_1_PATH, IOS_VOS_IB_2_PATH, IOS_VOS_IB_3_PATH
 import concurrent.futures
-
+import numpy as np
 
 
 inf = float('inf')
@@ -175,19 +175,7 @@ def generate_data_for_AD590(voltage, fluences_min, fluences_max):
         'I_out (µA)': ys
     }
 
-def extract_lm741_subcircuit(filled_in_netlist_str: str) -> str:
-    # Find the start and end of the subcircuit part in the filled-in netlist
-    start_marker = "*Subcircuit name: LM741"
-    end_marker = "*end of the subcircuit"
-    start_index = filled_in_netlist_str.find(start_marker)
-    end_index = filled_in_netlist_str.find(end_marker, start_index)
-    
-    # Extract the subcircuit part
-    subcircuit_content = filled_in_netlist_str[start_index:end_index + len(end_marker)]
-    
-    return subcircuit_content
-
-def generate_single_current_value_LM741(pnp_is: float, pnp_n: float, npn_is: float, npn_n: float, desired_voltage: float, testbench_path: Path) -> float:
+def generate_voltage_value_LM741(pnp_is: float, pnp_n: float, npn_is: float, npn_n: float, desired_voltage: float, testbench_path: Path) -> float:
     """This function is threadsafe and will work in the executable"""
     netlist_tempfile = tempfile.NamedTemporaryFile(delete=False)
     xyce_output_tempfile = tempfile.NamedTemporaryFile(delete=False)
@@ -204,22 +192,24 @@ def generate_single_current_value_LM741(pnp_is: float, pnp_n: float, npn_is: flo
             "NPN_N": npn_n
         }
         path_to_LM741_template = LM741_NETLIST_PATH
-        filled_in_netlist_str = process_file_with_replacements(path_to_LM741_template, d)
-        with open(testbench_path, 'r') as testbench_file:
-            testbench_content = testbench_file.read()
-            lm741_subcircuit_content = extract_lm741_subcircuit(filled_in_netlist_str)
-            testbench_content += '\n' + lm741_subcircuit_content
-        print(testbench_content)
-        write_string_to_file(temp_netlist_file_name, testbench_content)
+        testbench_content = read_file_as_string(testbench_path)
+        assert not is_error(testbench_content)
+
+        sub_circuit = read_file_as_string(path_to_LM741_template)
+        assert not is_error(testbench_content)
+
+        filled_in_netlist_str = format_string_with_dict(testbench_content + '\n' + sub_circuit, d)
+        write_string_to_file(temp_netlist_file_name , filled_in_netlist_str)
         path_to_xyce_exe = XYCE_EXE_PATH
         cmd_string = f"{path_to_xyce_exe} {temp_netlist_file_name}"
         stdout, stderr, return_code = run_command(cmd_string)
         out_text = read_file_as_string(temp_xyce_output_file_name)
-        print(out_text)
+        # breakpoint()
         out_data = parse_output_data(out_text)
-        for voltage, current in out_data:
-            if voltage == desired_voltage:
-                return current
+        #v8 and v3 are the left and right columns of the xyce output
+        for v8, v3 in out_data:
+            if v8 == desired_voltage:
+                return v3
         assert False
     finally:
         netlist_tempfile.close()
@@ -227,45 +217,93 @@ def generate_single_current_value_LM741(pnp_is: float, pnp_n: float, npn_is: flo
         os.remove(netlist_tempfile.name)
         os.remove(xyce_output_tempfile.name)
 
-def all_data_points_fluences_vs_current_LM741_parrallel(desired_voltage, testbench_path):
+def generate_3_voltages_LM741(pnp_is: float, pnp_n: float, npn_is: float, npn_n: float, desired_voltage: float):
+    vO1 = generate_voltage_value_LM741(
+            pnp_is = pnp_is,
+            pnp_n = pnp_n,
+            npn_is = npn_is,
+            npn_n = npn_n,
+            desired_voltage = desired_voltage,
+            testbench_path = IOS_VOS_IB_1_PATH
+    )
+    vO2 = generate_voltage_value_LM741(
+            pnp_is = pnp_is,
+            pnp_n = pnp_n,
+            npn_is = npn_is,
+            npn_n = npn_n,
+            desired_voltage = desired_voltage,
+            testbench_path = IOS_VOS_IB_2_PATH
+    )
+    vO3 = generate_voltage_value_LM741(
+            pnp_is = pnp_is,
+            pnp_n = pnp_n,
+            npn_is = npn_is,
+            npn_n = npn_n,
+            desired_voltage = desired_voltage,
+            testbench_path = IOS_VOS_IB_3_PATH
+    )
+    return vO1, vO2, vO3
+
+def matrix_math_LM741(vO1: float, vO2: float, vO3: float):
+    R1 = 10e6
+    R2 = 1e6
+    R3 = 9e6
+    
+    v_out = np.array([[vO1],[vO2],[vO3]])
+    matrix = np.array([
+        [-(R1/R2+1), R1-R3*(R1/R2+1), (R1+R3*(R1/R2+1))/2],
+        [-1, -R3, R3/2],
+        [-(R1/R2+1), R1, R1/2],
+    ])
+
+    x_vector = np.linalg.solve(matrix,v_out)
+
+    vos_Iib_Ios = x_vector[0,0], x_vector[1,0], x_vector[2,0]
+
+    return vos_Iib_Ios
+
+def all_data_points_fluences_vs_current_LM741(desired_voltage):
     npn_path = exe_tools.adjust_path('csvs/NPN_diode_parameters_V0.csv')
     pnp_path = exe_tools.adjust_path('csvs/PNP_diode_parameters_V0.csv')
     npn_df = pd.read_csv(npn_path)
     pnp_df = pd.read_csv(pnp_path)
 
-    def process_row(row_npn, row_pnp, desired_voltage):
+    for (idx_npn, row_npn),(idx_pnp, row_pnp) in zip(npn_df.iterrows(), pnp_df.iterrows()):
         avg_fluences = (row_npn['fluences (n/cm^2)'] + row_pnp['fluences (n/cm^2)']) / 2
-        current = generate_single_current_value_LM741(
+        vO1, vO2, vO3 = generate_3_voltages_LM741(
             pnp_is=row_pnp['Is'],
             pnp_n=row_pnp['n'],
             npn_is=row_npn['Is'],
             npn_n=row_npn['n'],
-            desired_voltage=desired_voltage,
-            testbench_path = testbench_path
+            desired_voltage=desired_voltage
         )
-        return (avg_fluences, current)
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        tasks_args = [(row_npn, row_pnp, desired_voltage) for (_, row_npn), (_, row_pnp) in zip (npn_df.iterrows(), pnp_df.iterrows())]
-        results = list(executor.map(lambda args: process_row(*args), tasks_args))
-        return results
+        vos,Iib,Ios = matrix_math_LM741(vO1,vO2,vO3)
+        yield {
+            'fluences': avg_fluences,
+            'V_os': vos,
+            'I_b': Iib,
+            'I_os': Ios
+        }
 
-def generate_data_for_LM741(voltage, fluences_min, fluences_max):
+    pass
+def generate_data_for_LM741(voltage: float, fluences_min, fluences_max, specification: str):
     xs = []
     ys = []
-    for fluences, current in all_data_points_fluences_vs_current_LM741_parrallel(voltage,IOS_VOS_IB_1_PATH):
+    for data_point in all_data_points_fluences_vs_current_LM741(voltage):
+        fluences = data_point['fluences']
         if fluences_min <= fluences <= fluences_max:
             xs.append(fluences)
-            ys.append(current * 10 ** 6) # convert amps to micro amps
+            ys.append(data_point[specification])
     return {
         'Fluences (n/cm^2)': xs,
-        'I_out (µA)': ys
+        specification: ys
     }
 
 
 def main():
     # data = generate_data_for_AD590(voltage=5.0, fluences_min=-inf, fluences_max=inf)
-    data = generate_data_for_LM741(voltage=5.0, fluences_min=-inf, fluences_max=inf)
+    data = generate_data_for_LM741(voltage=15.0, fluences_min=-inf, fluences_max=inf,specification= "V_os")
     (x_axis_name, x_axis_data), (y_axis_name, y_axis_data) = data.items()
     print(f"{x_axis_name}\t{y_axis_name}")
     for i in range(5):
